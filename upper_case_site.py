4813e4f7b25cc045fcdd14d3390587132eb4f7a3
SRIKALEESWARAR-S <srikaleeswarar675@gmail.com>
2025-09-20 21:59:27 +0530

    andys corner module

---
+++ upper_case_site.py
@@ -0,0 +1,4 @@
+sites = ['ariviyal palagai' ,'kaniyam','tamil computer','blog','vallamai','bookday','puthiya thendral','ariviyal vithai']
+upper_sites = [ name.upper() for name in sites]
+for name in upper_sites:
+    print(upper_sites)
