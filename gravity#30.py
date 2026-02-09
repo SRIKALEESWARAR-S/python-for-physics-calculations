cabba5c887410988769e9e3c48039530c97e2ca7
SRIKALEESWARAR-S <srikaleeswarar675@gmail.com>
2026-02-09 21:49:17 +0530

    30th formula

---
+++ gravity#30.py
@@ -0,0 +1,37 @@
+import numpy as np
+def gravitional_force(g,mass,m,r):
+    """_summary_
+
+    Parameters
+    ----------
+    g : _type_
+        _description_
+    mass : _type_
+        _description_
+    m : _type_
+        _description_
+    r : _type_
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+
+    Raises
+    ------
+    ValueError
+        _description_
+    """
+   
+    if isinstance(mass,(int,float)) and isinstance(g,(int,float)) and isinstance(m,(int,float)) and isinstance(r,(int,float)):
+        if m <= 0 or r <= 0 or mass <= 0 or g <= 0:
+            raise ValueError("The gravity cant be zero less than zero and radius r, masses should not be 0 ")
+        return (g*mass*m)/r
+    mass = np.array(mass,dtype=float)
+    r = np.array(r,dtype=float)
+    m = np.array(m,dtype=float)
+    g = np.array(g,dtype=float)
+    return (g*mass*m)/r
+
+            
